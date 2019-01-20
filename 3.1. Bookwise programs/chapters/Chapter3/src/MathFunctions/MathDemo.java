3ec8131825edd7050ee7c2472a87486699ffdf3c
shshetu <shshetu2017@gmail.com>
2019-01-20 14:01:54 +0600

    java

---
+++ 3.1. Bookwise programs/chapters/Chapter3/src/MathFunctions/MathDemo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MathFunctions;
+
+/**
+ *
+ * @author shshe
+ */
+import java.util.*;
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println("Round: "+Math.round(2.5));
+        System.out.println("Round: "+Math.round(-2.5));
+        System.out.println("Round: "+Math.round(-2.51));
+    }
+}
